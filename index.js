596cd398540249fc8905a94cb20d826b392a9628
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-02-23 17:49:58 -0300

    Pegando o valor do peso.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function calcularImc() {
 	var alturaDigitada = document.getElementById("altura");
+	var pesoDigitado = document.getElementById("peso");
 
 	var altura = alturaDigitada.value;
+	var peso = pesoDigitado.value;
 
 	console.log(altura);
+	console.log(peso);
 }
